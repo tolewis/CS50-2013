636480b17b1213d8f440a9ef7f80a9e7f52b192b
tolewis <tolewis@users.noreply.github.com>
2014-03-04 20:56:46 -0600

    Create generate.c

---
+++ ProblemSets/pset3/generate.c
@@ -0,0 +1,52 @@
+/**
+ * generate.c
+ *
+ * Computer Science 50
+ * Problem Set 3
+ *
+ * Generates pseudorandom numbers in [0,LIMIT), one per line.
+ *
+ * Usage: generate n [s]
+ *
+ * where n is number of pseudorandom numbers to print
+ * and s is an optional seed
+ */
+       
+#include <cs50.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define LIMIT 65536
+
+int main(int argc, string argv[])
+{
+    // Check number of arguments and remind usage sytax
+    if (argc != 2 && argc != 3)
+    {
+        printf("Usage: ./generate n [s]\n");
+        return 1;
+    }
+
+    // convert string from commandline argument  to integer
+    int n = atoi(argv[1]);
+
+    // provide pseudorandom numbers, evalute seed'ed argument first
+    if (argc == 3)
+    {
+        srand((unsigned int) atoi(argv[2]));
+    }
+    else
+    {
+        srand((unsigned int) time(NULL));
+    }
+
+    // loop to print "n" random numbers as defined in the inital command line arguments
+    for (int i = 0; i < n; i++)
+    {
+        printf("%i\n", rand() % LIMIT);
+    }
+
+    // that's all folks
+    return 0;
+}
