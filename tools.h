fb6354f895f76ffce226dd0ff84d8b4c87a9ed67
tolewis <tolewis@users.noreply.github.com>
2014-03-04 21:14:16 -0600

    Create tools.h

---
+++ tools.h
@@ -0,0 +1,19 @@
+/**
+*
+* Timothy Lewis | tim@tolewis.com
+*
+* List of Functions
+*  - Binary Search
+*  - Selection Sort
+*
+*/
+
+#include <stdio.h>
+#include <stdbool.h>
+#include <string.h>
+
+// Binary Search
+bool binarySearch(int value, int values[], int n);
+
+// Selection Sort
+void selectionSort(int values[], int n);
